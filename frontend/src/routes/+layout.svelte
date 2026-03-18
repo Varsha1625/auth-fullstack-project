39f402c081567b2684ab2652eb2fcd22d6975472
Varsha Nagwani <nagwanivarsha02@gmail.com>
2026-03-18 23:39:39 +0530

    fix:reset password redirect

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
 	import "../app.css";
 	import favicon from '$lib/assets/favicon.svg';
+
+	import {onMount} from "svelte";
+	import {goto} from "$app/navigation";
+
+    onMount(() => {
+		const hash = window.location.hash;
+
+		//Detect supabase password recovery
+		if(hash && hash.includes("type=recovery")){
+			goto("/reset-password");
+		}
+	});
+
 </script>
 
 <svelte:head>
